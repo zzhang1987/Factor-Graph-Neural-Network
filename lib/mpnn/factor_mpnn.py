6f54d56f6c4364b789b8684a897a5642d4ca2583
zhen <zhen@zzhang.org>
2019-05-27 11:07:09 +0800

    documentaton

---
+++ lib/mpnn/factor_mpnn.py
@@ -0,0 +1,85 @@
+import torch
+from .mp_nn import mp_conv_v2
+from .base_model import iid_mapping
+from .mp_nn_residual import mp_conv_residual
+from .base_model import base_mp_nn
+
+
+class factor_mpnn(torch.nn.Module):
+    def __init__(self,
+                 node_feature_dim,
+                 factor_feature_dim_list,
+                 dim_mapping_list,
+                 netype_list,
+                 gnn_immediate_dim=64,
+                 max_mpnn_dim=64):
+        super(factor_mpnn, self).__init__()
+        self.node_feature_dim = node_feature_dim
+        self.map_dim = dim_mapping_list[0]
+
+        self.mapping_modules = [
+            iid_mapping(self.node_feature_dim, self.map_dim)
+        ]
+
+        self.nfactor_types = len(factor_feature_dim_list)
+
+        for dim in factor_feature_dim_list:
+            self.mapping_modules.append(iid_mapping(dim, self.map_dim))
+
+        for idx, m in enumerate(self.mapping_modules):
+            self.add_module('mapping_modules_{}'.format(idx), m)
+
+        self.mp_nn_modules = []
+        self.mp_merge_modules = []
+        for idx in range(len(dim_mapping_list) - 1):
+            cmodule = []
+            nin = dim_mapping_list[idx]
+            nout = dim_mapping_list[idx + 1]
+
+            for jdx in range(self.nfactor_types):
+
+                if nin == nout:
+                    cmp_nn = mp_conv_residual(nin, gnn_immediate_dim,
+                                              netype_list[jdx])
+                else:
+                    if nin <= max_mpnn_dim and nout <= max_mpnn_dim:
+                        cmp_nn = mp_conv_v2(nin, nout, netype_list[jdx])
+                    else:
+                        cmp_nn = torch.nn.Sequential(
+                            torch.nn.Conv2d(nin, nout, 1),
+                            torch.nn.BatchNorm2d(nout),
+                            torch.nn.ReLU(inplace=True))
+
+                self.add_module('mp_nn_{}_{}'.format(idx, jdx), cmp_nn)
+                cmodule.append(cmp_nn)
+            self.mp_nn_modules.append(cmodule)
+            merge_module = iid_mapping(nout * self.nfactor_types, nout)
+            self.add_module('merge_module_{}'.format(idx), merge_module)
+            self.mp_merge_modules.append(merge_module)
+
+    def forward(self, node_features, factor_features, graph_structures):
+        nnode = node_features.shape[2]
+        nfeatures = self.mapping_modules[0](node_features)
+
+        ffeatures = [
+            m(f) for f, m in zip(factor_features, self.mapping_modules[1:])
+        ]
+
+        for midx, modules in enumerate(self.mp_nn_modules):
+            cnfeatures = []
+            cffeatures = []
+            for fidx, (f, m) in enumerate(zip(ffeatures, modules)):
+                cfeature = torch.cat([nfeatures, f], dim=2)
+                nn_idx, etype = graph_structures[fidx]
+                if isinstance(m, base_mp_nn):
+                    cfeature = m(cfeature.contiguous(), nn_idx, etype)
+                else:
+                    cfeature = m(cfeature.contiguous())
+
+                cnfeatures.append(cfeature[:, :, :nnode, :])
+                cffeatures.append(cfeature[:, :, nnode:, :])
+            cnfeatures = torch.cat(cnfeatures, dim=1)
+            nfeatures = self.mp_merge_modules[midx](cnfeatures)
+            ffeatures = cffeatures
+
+        return nfeatures, ffeatures
